e9fd2fbe5dd9d438f5ad674a68fef3d5790e162d
rishibodake <rishibodake@gmail.com>
2020-03-09 12:01:10 +0530

    Added:ExactWinAt100Feature

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,7 +5,54 @@
 
 startPosition=0
 finishPosition=100
+currentPosition=0
+priviousPosition=0
+#dicePos=0
 function rollDice(){
-	echo $((RANDOM%6+1))
+	dicePos=$((RANDOM%6+1))
+#	echo $dicePos
 }
-rollDice
+function snake(){
+	if [[ $currentPosition -le 0 ]]
+	then
+		currentPosition=$currentPosition
+	else
+		currentPosition=$(($currentPosition-$dicePos))
+	fi
+}
+function ladder(){
+		priviousPosition=$currentPosition
+		currentPosition=$(($currentPosition+$dicePos))
+#		if [[ $priviousPosition -eq 100 ]]
+#      then
+#				exit
+#      fi
+
+
+}
+function noPlay(){
+	currentPosition=$(($currentPosition+0))
+}
+function play(){
+	while [[ $currentPosition -lt $finishPosition ]]
+	do
+		choice=$((RANDOM%3))
+		rollDice
+		case $choice in
+						0)
+							noPlay;;
+						1)
+							snake;;
+						2)
+								ladder
+							;;
+						*)
+							echo Wrong
+		esac
+	 if [[ $currentPosition -gt $finishPosition ]]
+    then
+    	  currentPosition=$priviousPosition
+    fi
+done
+}
+play
