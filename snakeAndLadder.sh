dc5ccae257767cc7ed7b1674dd37dce625eaa9b8
rishibodake <rishibodake@gmail.com>
2020-03-09 10:35:26 +0530

    Added:CheckForOptionFeature

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,7 +5,38 @@
 
 startPosition=0
 finishPosition=100
+currentPosition=0
+#dicePos=0
 function rollDice(){
-	echo $((RANDOM%6+1))
+	dicePos=$((RANDOM%6+1))
+#	echo $dicePos
 }
-rollDice
+function snake(){
+	if [[ $currentPosition -le 0 ]]
+	then
+		currentPosition=0
+	else
+		currentposition=$(($currentPosition-$dicePos))
+	fi
+}
+function ladder(){
+	currentPosition=$(($currentPosition+$dicePos))
+}
+function noPlay(){
+	currentPosition=$(($currentPosition+0))
+}
+function play(){
+	choice=$((RANDOM%3))
+	rollDice
+	case $choice in 
+						0)
+							noPlay;;
+						1)
+							snake;;
+						2)
+							ladder;;
+						*)
+							echo Wrong
+	esac
+}
+play
