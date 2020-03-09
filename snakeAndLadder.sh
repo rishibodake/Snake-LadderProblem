1bc7ab5731e89e4f3f3fba1761ab01904292d031
rishibodake <rishibodake@gmail.com>
2020-03-09 12:06:14 +0530

    Added:DiceOutcomeFeature

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,10 +2,59 @@
 #Discription:Snake & Ladder as per usercases
 #Author:Hrishikesh M Bodake
 #Since:March 7 2020 / Saturday
-
+diceCounter=0
 startPosition=0
 finishPosition=100
+currentPosition=0
+priviousPosition=0
+#dicePos=0
 function rollDice(){
-	echo $((RANDOM%6+1))
+	dicePos=$((RANDOM%6+1))
+#	echo $dicePos
 }
-rollDice
+function snake(){
+	if [[ $currentPosition -le 0 ]]
+	then
+		currentPosition=$currentPosition
+	else
+		currentPosition=$(($currentPosition-$dicePos))
+	fi
+}
+function ladder(){
+		priviousPosition=$currentPosition
+		currentPosition=$(($currentPosition+$dicePos))
+#		if [[ $priviousPosition -eq 100 ]]
+#      then
+#				exit
+#      fi
+
+
+}
+function noPlay(){
+	currentPosition=$(($currentPosition+0))
+}
+function play(){
+	while [[ $currentPosition -lt $finishPosition ]]
+	do
+		choice=$((RANDOM%3))
+		rollDice
+		((diceCounter++))
+		echo diceOucome is $dicePos
+		case $choice in
+						0)
+							noPlay;;
+						1)
+							snake;;
+						2)
+								ladder
+							;;
+						*)
+							echo Wrong
+		esac
+	 if [[ $currentPosition -gt $finishPosition ]]
+    then
+    	  currentPosition=$priviousPosition
+    fi
+done
+}
+play
